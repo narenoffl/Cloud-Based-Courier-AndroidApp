7df1764349704882b36d00fc2dc24a5133704c41
Naren <naren.p.2018.cse@ritchennai.edu.in>
2021-04-19 13:52:15 +0530

    ScheduledDelivery Activity

--- app/src/main/java/com/example/couriercloudapp/HomePage.java
+++ app/src/main/java/com/example/couriercloudapp/HomePage.java
@@ -2,13 +2,29 @@ package com.example.couriercloudapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    Button scheduled_delivery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        scheduled_delivery = findViewById(R.id.buttonScheduled);
+        scheduled_delivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scheduledDelView = new Intent(HomePage.this, Schedule.class);
+                startActivity(scheduledDelView);
+            }
+        });
+
+
     }
 }
